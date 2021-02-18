62359cbb98b1ef24dac736ea5c13800cc8e120de
Josh Bernstein <joshua.bernstein@outlook.com>
2021-02-17 19:00:19 -0700

    Added a basic table for tasks

--- pages/projects/[projectId].tsx
+++ pages/projects/[projectId].tsx
@@ -6,6 +6,7 @@ import { useMST } from "../_app";
 import { BreadCrumb } from "../../components/BreadCrumb";
 import { observer } from "mobx-react";
 import { INote, IProject } from "../../models/Project";
+import { DP } from "../../components/Dark";
 
 const ProjectPage: Page = () => {
   const store = useMST();
@@ -29,11 +30,10 @@ const ProjectPage: Page = () => {
         <title>{projectDetails.name}</title>
       </Head>
 
+      {/* BODY */}
       <div className='absolute w-full h-48 bg-gradient-to-b from-purple-900 to-transparent'></div>
 
-      {/* BODY */}
       <div className='px-4 mx-auto sm:px-6 md:px-8'>
-        {/* <!-- Replace with your content --> */}
         <input
           type='text'
           name='title'
@@ -42,18 +42,82 @@ const ProjectPage: Page = () => {
           onChange={onProjectRename}
           className='w-full px-2 mt-12 -mx-2 text-5xl font-semibold text-white bg-transparent border-none outline-none opacity-h-emp focus:outline-none'
         ></input>
-        <div className='py-4 text-white opacity-l-emp'>
-          {projectDetails.allTasks().map((task: INote) => {
-            return (
-              <div className={`flex flex-inline`}>
-                <div className={`w-96`}>{task.text}</div>
-                <div className={`w-48`}>{task.assignedTo}</div>
-                <div className={`w-48`}>{task.createdOn.toDateString()}</div>
+
+        <div className='flex flex-col'>
+          <div className='z-10 py-4 overflow-x-auto'>
+            <div className='inline-block min-w-full align-middle sm:px-6 lg:px-8'>
+              <div className='overflow-hidden border-b border-gray-900 shadow sm:rounded-lg'>
+                <table className='w-full divide-y divide-gray-900 table-fixed'>
+                  <thead className={DP.dp12}>
+                    <tr>
+                      <th
+                        scope='col'
+                        className='px-6 py-3 text-xs font-medium tracking-wider text-left text-center text-white uppercase w-28 w-30 opacity-lemp'
+                      >
+                        Status
+                      </th>
+                      <th
+                        scope='col'
+                        className='w-1/3 px-6 py-3 text-xs font-medium tracking-wider text-left text-white uppercase opacity-lemp'
+                      >
+                        Description
+                      </th>
+                      <th
+                        scope='col'
+                        className='px-6 py-3 text-xs font-medium tracking-wider text-left text-white uppercase opacity-lemp'
+                      >
+                        Assigned
+                      </th>
+                      <th
+                        scope='col'
+                        className='px-6 py-3 text-xs font-medium tracking-wider text-left text-white uppercase opacity-lemp'
+                      >
+                        Due
+                      </th>
+                      <th scope='col' className='relative w-1/5 px-6 py-3'>
+                        <span className='sr-only'>Edit</span>
+                      </th>
+                    </tr>
+                  </thead>
+                  <tbody className={`divide-y divide-gray-900 ${DP.dp16}`}>
+                    {projectDetails.allTasks().map((task: INote) => {
+                      return (
+                        <tr>
+                          <td className='text-sm font-medium text-white opacity-h-emp whitespace-nowrap'>
+                            <label className='inline-flex w-full'>
+                              <input
+                                type='checkbox'
+                                className='w-5 h-5 m-auto text-purple-600 bg-gray-200 rounded form-checkbox'
+                                
+                              />
+                            </label>
+                          </td>
+                          <td className='px-6 py-4 text-sm text-white opacity-h-emp'>
+                            {task.text}
+                          </td>
+                          <td className='px-6 py-4 text-sm text-white opacity-h-emp whitespace-nowrap'>
+                            {task.assignedTo}
+                          </td>
+                          <td className='px-6 py-4 text-sm text-white opacity-h-emp whitespace-nowrap'>
+                            {task.createdOn.toDateString()}
+                          </td>
+                          <td className='px-6 py-4 text-sm font-medium text-right'>
+                            <a
+                              href='#'
+                              className='text-indigo-600 hover:text-indigo-900'
+                            >
+                              Edit
+                            </a>
+                          </td>
+                        </tr>
+                      );
+                    })}
+                  </tbody>
+                </table>
               </div>
-            );
-          })}
+            </div>
+          </div>
         </div>
-        {/* <!-- /End replace --> */}
       </div>
     </div>
   ) : (
